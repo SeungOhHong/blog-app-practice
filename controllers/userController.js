c368d533eb74ced8afb86009f29b3ed154f90471
SeungOhHong <ghdtmddh@gmail.com>
2023-03-15 23:04:12 +0900

    feat: cleanUp 정돈 함수를 추가하기

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,23 @@
-const User = require("../models/User");
+const User = require('../models/User')
 
-exports.login = function () {};
+exports.login = function() {
 
-exports.logout = function () {};
+}
 
-exports.register = function (req, res) {
-  let user = new User(req.body);
-  user.register();
-  // 만약 에러 배열이 존재한다면
+exports.logout = function() {
+  
+}
+
+exports.register = function(req, res) {
+  let user = new User(req.body)
+  user.register()
   if (user.errors.length) {
-    // 유저에게 에러 메세지를 보낸다
-    res.send(user.errors);
-    // 에러 배열의 길이가 0이라면(없다면) 에러가 없다고 표시해준다
+    res.send(user.errors)
   } else {
-    res.send("Congrats, there are no errors.");
+    res.send("Congrats, there are no errors.")
   }
-};
+}
 
-exports.home = function (req, res) {
-  res.render("home-guest");
-};
+exports.home = function(req, res) {
+  res.render('home-guest')
+}
--- models/User.js
+++ models/User.js
@@ -1,68 +1,75 @@
-// npm install validator
-// validator 를 임포트 해준다. 이 모듈은 정규식 처럼 유효성 검사를 도와준다
 const validator = require("validator");
 
 let User = function (data) {
   this.data = data;
-  // 에러 객체를 초기화 한다
   this.errors = [];
 };
 
+// cleanUp 함수를 상속 시켜준다(유효성 검사 전에 미리 정돈)
+User.prototype.cleanUp = function () {
+  // typeof 함수는 해당 값의 타입을 반환한다
+  // 만약 문자열이 아니라면
+  if (typeof this.data.username != "string") {
+    // 유효성 검사 이전에 문자열이 아닌 값을 미리 공백으로 대체해서 무시해준다
+    // validation 함수가 불필요하게 체크하는 것을 방지
+    this.data.username = "";
+  }
+  if (typeof this.data.email != "string") {
+    this.data.email = "";
+  }
+  if (typeof this.data.password != "string") {
+    this.data.password = "";
+  }
+
+  // 만약 모두 문자열이라면
+  // get rid of any bogus properties (data 속성을 덮어씌워서 제거해버린다)
+  this.data = {
+    // 만약 color : red 와 같은 속성이 있다면 제거해버린다
+    // 동시에 불필요한 공백을 트림하고 소문자로 바꿔준다
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password,
+  };
+};
+
 User.prototype.validate = function () {
-  // 만약 유저 이름이 공백이라면
   if (this.data.username == "") {
-    // user 객체 내의 errors 배열에 해당 메세지를 푸시한다
-    // 유저 컨트롤러로 가서 에러 유무에 따른 상태를 정의해준다
     this.errors.push("You must provide a username.");
   }
-  // 만약 문자 혹은 숫자가 아닐 경우에
   if (
     this.data.username != "" &&
     !validator.isAlphanumeric(this.data.username)
   ) {
-    // 에러 메세지를 푸시한다
     this.errors.push("Username can only contain letters and numbers.");
   }
-  // 만약 validator.isEmail 가 false를 리턴할 경우
   if (!validator.isEmail(this.data.email)) {
-    // 에러를 푸시한다
     this.errors.push("You must provide a valid email address.");
   }
-  // 패스워드 공백일 경우
   if (this.data.password == "") {
-    // 등록 할 수 없다는 에러를 표시한다
     this.errors.push("You must provide a password.");
   }
-
-  // 최소 패스워드 : 만약 패스워드가 0보다 크고 12자 보다 작을 경우에
   if (this.data.password.length > 0 && this.data.password.length < 12) {
-    // 등록할 수 없다는 에러를 표시한다
     this.errors.push("Password must be at least 12 characters.");
   }
-  // 최대 패스워드 : 패스워드가 100자를 넘는다면
   if (this.data.password.length > 100) {
-    // 등록할 수 없다는 에러를 표시한다
     this.errors.push("Password cannot exceed 100 characters.");
   }
-
-  // 최소 이름 자수 : 만약 유저 이름이 0보다 크고 3보다 작다면
   if (this.data.username.length > 0 && this.data.username.length < 3) {
-    // 등록 할 수 없다는 에러를 표시한다
     this.errors.push("Username must be at least 3 characters.");
   }
-  // 최대 이름 자수 : 만약 유저 이름이 30자를 넘어가면
   if (this.data.username.length > 30) {
-    // 등록 할 수 없다는 에러를 표시한다
     this.errors.push("Username cannot exceed 30 characters.");
   }
 };
 
 User.prototype.register = function () {
-  // 1단계: 유저 데이터 유효성 검사하기
-  // 2단계: 유효성에 문제가 없다면 (에러가 없다면) DB에 유저 데이터 저장하기
-
-  // 여기서 this 키워드가 가리키는 것은 유저가 제출한 데이터로 생성되는 user 객체이다
+  // cleanup 메서드를 추가해준다
+  // Step #1: Validate user data
+  this.cleanUp();
   this.validate();
+
+  // Step #2: Only if there are no validation errors
+  // then save the user data into a database
 };
 
 module.exports = User;
