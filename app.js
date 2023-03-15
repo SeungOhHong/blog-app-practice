069eeba23270f7f809879ab9c78ad7f157cb1ce1
SeungOhHong <ghdtmddh@gmail.com>
2023-03-15 21:38:24 +0900

    chore: 컨트롤러 셋팅하기

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-// 라우터를 불러와준다 (현재 경로를 적어준다)
-// require 함수는 해당 파일이 module.exports 하고 있는 것을 리턴해준다
-// 라우터에서 정의된 함수를 app.js 파일에서도 사용할 수 있게 된다
-const router = require("./router");
+const router = require('./router')
 
-app.use(express.static("public"));
-app.set("views", "views");
-app.set("view engine", "ejs");
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
 
-// 모든 라우터를 파일로 만들어서 관리한다 (router.js)
-// app.use 함수를 이용하여 base URL과 파일에서 임포트한 변수명을 b에 적어준다
-app.use("/", router);
+app.use('/', router)
 
-app.listen(3000);
+app.listen(3000)
--- router.js
+++ router.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
+// 컨트롤러를 require 함수로 임포트해서 컨트롤러에서 정의한 함수들을 모두 불러온다
+const userController = require("./controllers/userController");
 
-// base URL
-router.get("/", function (req, res) {
-  // 홈페이지 템플릿을 렌더링 해준다
-  res.render("home-guest");
-});
+// 해당 a URL로 요청이 들어오면 b가 실행된다
+// 라우터가 실제 함수를 포함하지 않고 action에 관한 함수를 파일로 따로 관리한다
+// 그것이 컨트롤러가 하는 일이다.
+// 라우터는 단순히 경로들을 리스팅하는 역할만 하도록 한다
+// controllers 폴더를 생성한 후 파일 3개를 생성한다.
 
-router.get("/about", function (req, res) {
-  // 이제 /about 페이지에 get요청을 보내면 아래의 메세지를 응답한다
-  res.send("This is our about page");
-});
+// userController 변수가 반환하는 객체 중 home 함수를 가져온다
+router.get("/", userController.home);
+router.post("/register", userController.register);
 
-// 파일 간에 코드를 공유하기 위해서 라우터를 export 해준다
-// 객체를 export 해 줄 수도 있다
 module.exports = router;
+
+// MVC 패턴
+// Model = 데이터
+// View = html 뷰파일
+// Controller = 들어온 요청에 따라서 적절한 model 을 불러온 후 view에 해당 다이나믹 데이터들을 통과 시켜준다
