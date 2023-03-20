50e2b8850ebfa3d74b70cfb3f014b457c1ad56e5
SeungOhHong <ghdtmddh@gmail.com>
2023-03-20 16:48:46 +0900

    feat : 환경변수로 비밀키 관리하기

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const router = require("./router");
+const router = require('./router')
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
-app.use(express.static("public"));
-app.set("views", "views");
-app.set("view engine", "ejs");
+app.use(express.static('public'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
 
-app.use("/", router);
+app.use('/', router)
 
-// 이제 포트를 직접 열지 않고 익스포트해준다 그 후 db.js 에서 불러온다
-module.exports = app;
+module.exports = app
--- models/User.js
+++ models/User.js
@@ -1,76 +1,45 @@
-// 새로운 DB 파일을 불러온다 그 후 users 컬렉션이 반환하는 데이터를 변수에 담는다
-const usersCollection = require("../db").collection("users");
-const validator = require("validator");
+const usersCollection = require('../db').collection("users")
+const validator = require("validator")
 
-let User = function (data) {
-  this.data = data;
-  this.errors = [];
-};
+let User = function(data) {
+  this.data = data
+  this.errors = []
+}
 
-User.prototype.cleanUp = function () {
-  if (typeof this.data.username != "string") {
-    this.data.username = "";
-  }
-  if (typeof this.data.email != "string") {
-    this.data.email = "";
-  }
-  if (typeof this.data.password != "string") {
-    this.data.password = "";
-  }
+User.prototype.cleanUp = function() {
+  if (typeof(this.data.username) != "string") {this.data.username = ""}
+  if (typeof(this.data.email) != "string") {this.data.email = ""}
+  if (typeof(this.data.password) != "string") {this.data.password = ""}
 
   // get rid of any bogus properties
   this.data = {
     username: this.data.username.trim().toLowerCase(),
     email: this.data.email.trim().toLowerCase(),
-    password: this.data.password,
-  };
-};
+    password: this.data.password
+  }
+}
 
-User.prototype.validate = function () {
-  if (this.data.username == "") {
-    this.errors.push("You must provide a username.");
-  }
-  if (
-    this.data.username != "" &&
-    !validator.isAlphanumeric(this.data.username)
-  ) {
-    this.errors.push("Username can only contain letters and numbers.");
-  }
-  if (!validator.isEmail(this.data.email)) {
-    this.errors.push("You must provide a valid email address.");
-  }
-  if (this.data.password == "") {
-    this.errors.push("You must provide a password.");
-  }
-  if (this.data.password.length > 0 && this.data.password.length < 12) {
-    this.errors.push("Password must be at least 12 characters.");
-  }
-  if (this.data.password.length > 100) {
-    this.errors.push("Password cannot exceed 100 characters.");
-  }
-  if (this.data.username.length > 0 && this.data.username.length < 3) {
-    this.errors.push("Username must be at least 3 characters.");
-  }
-  if (this.data.username.length > 30) {
-    this.errors.push("Username cannot exceed 30 characters.");
-  }
-};
+User.prototype.validate = function() {
+  if (this.data.username == "") {this.errors.push("You must provide a username.")}
+  if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+  if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
+  if (this.data.password == "") {this.errors.push("You must provide a password.")}
+  if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters.")}
+  if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
+  if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters.")}
+  if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
+}
 
-User.prototype.register = function () {
+User.prototype.register = function() {
   // Step #1: Validate user data
-  this.cleanUp();
-  this.validate();
+  this.cleanUp()
+  this.validate()
 
-  // Step #2: Only if there are no validation errors
+  // Step #2: Only if there are no validation errors 
   // then save the user data into a database
-  // 이제 에러배열이 비어있다면
   if (!this.errors.length) {
-    // crud 작업을 해준다. 몽고DB에 새로운 도큐먼트를 생성해준다
-    // 명령어는 insertOne() 이다
-    // 괄호 안에는 우리가 도큐먼트에 우리가 저장할 데이터를 저장한다 (this.data)
-    // 이제 계정을 생성하면 몽고DB에 저장이 된다
-    usersCollection.insertOne(this.data);
+    usersCollection.insertOne(this.data)
   }
-};
+}
 
-module.exports = User;
+module.exports = User
