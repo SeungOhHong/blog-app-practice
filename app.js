0ffa9afa8134dbe77b634eefbab2042a9e2be1a8
SeungOhHong <ghdtmddh@gmail.com>
2023-03-20 17:53:02 +0900

    chore : mysql 연동하기

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const express = require('express')
-const app = express()
+// npm install --save mysql2
+const express = require("express");
+// db 풀을 임포트해준다
+const db = require("./util/database");
+const app = express();
 
-const router = require('./router')
+const router = require("./router");
+// db.execute  mysql에서 테이블 내의 모든 쿼리를 불러온다
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result[0], result[1]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
-app.set('views', 'views')
-app.set('view engine', 'ejs')
+app.use("/", router);
 
-app.use('/', router)
-
-module.exports = app
+module.exports = app;
---
+++ controllers/shop.js
@@ -0,0 +1,84 @@
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/product-list", {
+        prods: rows,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    // then / catch 문을 이용해서 처리해준다
+    .then(([product]) => {
+      res.render("shop/product-detail", {
+        // 배열의 첫번째 요소를 불러와준다
+        product: product[0],
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/index", {
+        prods: rows,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+exports.getCart = (req, res, next) => {
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cartProducts,
+      });
+    });
+  });
+};
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
+};
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
