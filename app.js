4a540ca5b97a0c7e37ec8a3f3d22b03f95e5974f
SeungOhHong <ghdtmddh@gmail.com>
2023-03-27 18:08:09 +0900

    feat : 세션 데이터 DB에 저장하고 해당 데이터를 이용해서 동적으로 데이터 변경하기

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
+/* 세션 데이터를 서버의 메모리에 저장하고 있었기 때문에 서버가 종료되거나 재시작될 때마다 해당 세션 데이터가 모두 손실됩니다.
+세션 데이터를 MongoDB 데이터베이스에 저장하도록 설정해 보겠습니다.
+이를 통해 세션 데이터를 손실하지 않고 노드 서버를 다시 시작할 수 있습니다. */
+
 const express = require("express");
-// 세션 패키지 익스포트하기
 const session = require("express-session");
+// npm install connect-mongo
+// 세션옵션을 사용하기 위한 connect-mongo 패키지를 임포트한다
+const MongoStore = require("connect-mongo");
 const app = express();
 
-// 어떻게 요청을 신뢰할 수 있을까?
-// 1. 세션 2. 토큰
-// npm install express-session
-// session() 안에 객체로 설정해준다
-// 보일러플레이트 코드이다(외울필요없음)
 let sessionOptions = session({
-  secret: "JavaScript 굳~",
+  secret: "JavaScript is sooooooooo coool",
+  // 기본적으로 세션 데이터는 서버 메모리에 저장되는 것이 디폴트이지만
+  // 해당 데이터를 몽고DB에 저장하기 위해서 store 프로퍼티를 추가해준다
+  // 우리는 이미 DB에 연동하는 파일을 생성하였기 때문에
+  // MongoStore.create() 안에 {}객체 임포트 해주면 된다
+
+  // 하지만 현재 MongoDB 클라이언트가 아닌 데이터베이스를 내보내도록 설정되어 있기 떄문에
+  // 이제 db.js 파일로 가서 픽스해준다
+  store: MongoStore.create({ client: require("./db") }),
   resave: false,
   saveUninitialized: false,
-  // maxAge : 얼마동안 valid 할 것인가(밀리세컨드 단위이다)
-  // 1000 = 1초 / 60 : 1분 /  60 : 1시간 / 24 : 하루    => 1일 후에 expire되는 세션쿠키 발행
   cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
 });
 
-// 개발자 도구의 Application 탭에 들어가보면 쿠키를 확인할 수 있다.
-
-// 미들웨어 연결해주기
 app.use(sessionOptions);
 
 const router = require("./router");
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,22 +5,7 @@ exports.login = function (req, res) {
   user
     .login()
     .then(function (result) {
-      // 세션 객체에 user라는 객체 프로퍼티를 추가한다
-      // HTTP 요청은 stateless하기 때문에 이전 요청에 대한 기억을 가지고 있지 않다
-      // 그래서 세션이라는 개념으로 데이터가 지속성을 가지도록 만들어주는 것이다
-      // 이제 이 세션 데이터를 URL들과 라우터에서 확인할 수 있다
       req.session.user = { favColor: "blue", username: user.data.username };
-      /* 
-      방문자가 올바른 사용자 이름과 비밀번호 값을 입력했을 때, 즉 이 코드가 잘 실행되었다는 것을 의미합니다. 여기서 세션 객체를 활용했을 때, 두 가지 일이 발생한다.
-
-첫째, 서버는 이 세션 데이터를 메모리에 저장한다.
-둘째, 세션 패키지가 수행할 작업은 웹 브라우저가 쿠키를 만들도록 지시한다.
-      
- 이제 localhost를 방문할 때마다.
-Google Chrome 웹 브라우저에서 이 쿠키와 값을 노드 서버로 다시 보냅니다.
-우리의 서버는 쿠키에 저장된  고유한 식별자 값을 볼 것이고 우리의 서버는 "아하"라고 말할 것입니다.
-그래서 서버는 이 세션 값이 사용자 또는 사용자가 올바른 사용자 이름과 암호를 입력한 동일한 사용자 또는 동일한 웹 브라우저라는 것을 의미한다고 말합니다
-      */
       res.send(result);
     })
     .catch(function (e) {
@@ -41,12 +26,14 @@ exports.register = function (req, res) {
 };
 
 exports.home = function (req, res) {
-  // 만약 현재 방문자 또는 브라우저에 세션 데이터(req.session.user)가 연결되어 있으면 응용 프로그램에 대해 안녕하세요 및 환영합니다라는 사용자 지정 메시지를 보냅니다.
   if (req.session.user) {
-    res.send("환영합니다!");
+    // 이제 세션 데이터가 있을 경우(로그인 상태) 다른 페이지를 보여주도록 한다
+    // 하지만 유저에 따라서 플레이스홀더에 다른 이름이 보여야 하기 때문에
+    // render(a,b)  a = 렌더하고자 하는 템플릿 , b = 템플릿에 전달하고 싶은 데이터 객체
+    // 세션에 저장된 유저 이름을 가져온다
+    // 템플릿에 하드코딩된 부분을 <%= username %> 으로 동적인 데이터로 변경해준다
+    res.render("home-dashboard", { username: req.session.user.username });
   } else {
-    // 세션 데이터가 없다면 게스트 홈페이지를 송출한다
     res.render("home-guest");
-    // 즉, 로그인한 사용자와 게스트 사용자를 브라우저에서 세션 데이터로 구분할 수 있게 된다.
   }
 };
