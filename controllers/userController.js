a1e9220abb2d25726c8e920415020c80535fb8a2
SeungOhHong <ghdtmddh@gmail.com>
2023-03-27 20:09:13 +0900

    feat : 유저의 이메일 따라서 아바타를 바꿔주는 기능 추가하기

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,9 @@ exports.login = function (req, res) {
   user
     .login()
     .then(function (result) {
-      req.session.user = { favColor: "blue", username: user.data.username };
+      // 세션에 avatar 를 저장하도록 한다
+      // 이제 유저가 로그인이 돼있다면 아바타를 재생성하지 않는다
+      req.session.user = { avatar: user.avatar, username: user.data.username };
       req.session.save(function () {
         res.redirect("/");
       });
@@ -26,27 +28,16 @@ exports.logout = function (req, res) {
 
 exports.register = function (req, res) {
   let user = new User(req.body);
-  // 이제 user.register 함수는 비동기 함수이기 때문에
-  // register 함수의 에러 여부에 따라서 다음 라인 실행 여부를 then catch로 처리해줘야한다
-  // 이 말은 User.js에서 register함수가 Promise를 리턴하도록 해줘야한다는 의미이다
   user
     .register()
-    // user.register() 함수는 Promise를 리턴한다 .then().catch 문을 이용한다
-    // user.register().then().catch()
-    // 복잡해보이지만 user.register().then(()=>{}).catch(()=>{}) 이런 형태이다
-    // promise 가 reject 됐을 경우에는 catch 객체를 반환한다
     .then(() => {
-      // Promise가 해결 됐을 때
-      // 세션 데이터를 로그인 상태로 업데이트 한 후
-      req.session.user = { username: user.data.username };
+      // 세션에 avatar 를 저장하도록 한다
+      req.session.user = { username: user.data.username, avatar: user.avatar };
       req.session.save(function () {
-        // 로그인 상태인 베이스 url로 리다이렉트
         res.redirect("/");
       });
     })
-    // Promise 가 미해결 됐을 때
     .catch((regErrors) => {
-      // Promise로 리턴되어 들어온 regErrors 배열을 참조한다
       regErrors.forEach(function (error) {
         req.flash("regErrors", error);
       });
@@ -58,7 +49,11 @@ exports.register = function (req, res) {
 
 exports.home = function (req, res) {
   if (req.session.user) {
-    res.render("home-dashboard", { username: req.session.user.username });
+    res.render("home-dashboard", {
+      username: req.session.user.username,
+      // 로그인상태인 경우 아바타를 렌더링해준다
+      avatar: req.session.user.avatar,
+    });
   } else {
     res.render("home-guest", {
       errors: req.flash("errors"),
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs");
 const usersCollection = require("../db").db().collection("users");
 const validator = require("validator");
+// md5 패키지를 임포트해준다
+const md5 = require("md5");
 
 let User = function (data) {
   this.data = data;
@@ -27,9 +29,6 @@ User.prototype.cleanUp = function () {
 };
 
 User.prototype.validate = function () {
-  // validate 함수가 Promise를 리턴하도록 한다
-  // Promise 는 resolve 와 reject 두 개의 인자를 받는다
-  // 또한 화살표 함수를 이용해서 this 키워드가 함수 자기 자신을 가리키도록 해준다
   return new Promise(async (resolve, reject) => {
     if (this.data.username == "") {
       this.errors.push("You must provide a username.");
@@ -82,7 +81,6 @@ User.prototype.validate = function () {
         this.errors.push("That email is already being used.");
       }
     }
-    // 마지막 라인에 Promise가 해결됐음을 나타내는 resolve() 함수를 추가해준다
     resolve();
   });
 };
@@ -97,6 +95,11 @@ User.prototype.login = function () {
           attemptedUser &&
           bcrypt.compareSync(this.data.password, attemptedUser.password)
         ) {
+          // 아바타는 이메일과 연동 돼있기 때문에 이메일을 가져와야한다
+          // 사용자가 로그인할 사용자 이름과 암호만 입력하고 메일 주소는 입력하지 않았기 때문에 getAvatar 함수를 호출하기 전에 사용자의 메일 주소가 객체에 있는지 확인해야한다. 데이터베이스에서 해당 사용자 계정과 관련된 메일을 가져와야 합니다.
+          this.data = attemptedUser;
+          // 로그인에 성공할 경우에 객체 내부에 avatar 프로퍼티가 추가된다
+          this.getAvatar();
           resolve("Congrats!");
         } else {
           reject("Invalid username / password.");
@@ -109,16 +112,10 @@ User.prototype.login = function () {
 };
 
 User.prototype.register = function () {
-  // register 함수가 Promise를 리턴하도록 해준다
   return new Promise(async (resolve, reject) => {
     // Step #1: Validate user data
     this.cleanUp();
-    // 이제 validate 함수가 프로미스를 리턴하기 때문에 await 키워드를 이용한다
     await this.validate();
-    // 이로인해서 아래의 register에 관련된 코드 로직이 실행되기 전에 유효성 검사가 이루어질 것 이다.
-    // (유효성 검사가 끝나기 전에는(Promise를 리턴하기 전에) 회원가입과 관련된 함수가 실행되지 않는다)
-
-    // 이제 이 register함수가 어디서 호출 되는지 찾아보자 userController 에서 실행된다
 
     // Step #2: Only if there are no validation errors
     // then save the user data into a database
@@ -127,13 +124,29 @@ User.prototype.register = function () {
       let salt = bcrypt.genSaltSync(10);
       this.data.password = bcrypt.hashSync(this.data.password, salt);
       await usersCollection.insertOne(this.data);
-      // 에러가 없다면 resolve로 프로미스를 해결해준다
+      // 회원가입에 성공할 경우에도 유저 객체 안에 avatar 프로퍼티가 추가된다
+      // 해당 라인을 DB에 데이터를 저장한 이후에 넣는 이유는
+      // 아바타 url을 DB에 영구적으로 저장할 것이 아니기 때문이다
+      // 만약 아타바를 제공하는 사이트의 url이 변경되면 곤란해지기 때문이다
+      // 때문에 필요할 때에 url을 바로 생성하도록 하는 것이 좋다
+      // 이제 DB에 영구저장 되는 것이 아니라 메모리 상의 user 객체에 저장된다
+      this.getAvatar();
       resolve();
     } else {
-      // 에러가 있다면 현재 에러를 reject해준다
       reject(this.errors);
     }
   });
+};
+
+// 아바타를 가져오는 새로운 함수를 생성한다
+User.prototype.getAvatar = function () {
+  // 객체에 새로운 avatar 프로퍼티를 추가한다
+  // `https://gravatar.com/avatar/email?s=128`;
+  // 이 기본 형식에서 email을 md5 라이브러리를 이용해서 해싱한 후에
+  // 현재 사용자의 이메일로 바꿔주는 것이다
+  // npm install md5
+  // md5(현재 사용자)
+  this.avatar = `https://gravatar.com/avatar/${md5(this.data.email)}?s=128`;
 };
 
 module.exports = User;
