ada8b6ea7e7c0152e462a8e09764f2b552e955c1
SeungOhHong <ghdtmddh@gmail.com>
2023-03-28 16:13:02 +0900

    feat : 유저 마다 게시물 관리를 위해서 id 넘겨주고 몽고DB패키지의  ObjectId 이용하기

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,58 +1,77 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
-exports.mustBeLoggedIn = function(req, res, next) {
+exports.mustBeLoggedIn = function (req, res, next) {
   if (req.session.user) {
-    next()
+    next();
   } else {
-    req.flash("errors", "You must be logged in to perform that action.")
-    req.session.save(function() {
-      res.redirect('/')
-    })
+    req.flash("errors", "You must be logged in to perform that action.");
+    req.session.save(function () {
+      res.redirect("/");
+    });
   }
-}
+};
 
-exports.login = function(req, res) {
-  let user = new User(req.body)
-  user.login().then(function(result) {
-    req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
-    req.session.save(function() {
-      res.redirect('/')
+exports.login = function (req, res) {
+  let user = new User(req.body);
+  user
+    .login()
+    .then(function (result) {
+      req.session.user = {
+        avatar: user.avatar,
+        username: user.data.username,
+        // 생성 게시물을 유저별로 관리하기 위해서 세션 사용자 데이터에 유저의 id에 대한 정보도 저장되도록 한다
+        _id: user.data._id,
+      };
+      req.session.save(function () {
+        res.redirect("/");
+      });
     })
-  }).catch(function(e) {
-    req.flash('errors', e)
-    req.session.save(function() {
-      res.redirect('/')
-    })
-  })
-}
+    .catch(function (e) {
+      req.flash("errors", e);
+      req.session.save(function () {
+        res.redirect("/");
+      });
+    });
+};
 
-exports.logout = function(req, res) {
-  req.session.destroy(function() {
-    res.redirect('/')
-  })
-}
+exports.logout = function (req, res) {
+  req.session.destroy(function () {
+    res.redirect("/");
+  });
+};
 
-exports.register = function(req, res) {
-  let user = new User(req.body)
-  user.register().then(() => {
-    req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
-    req.session.save(function() {
-      res.redirect('/')
+exports.register = function (req, res) {
+  let user = new User(req.body);
+  user
+    .register()
+    .then(() => {
+      req.session.user = {
+        username: user.data.username,
+        avatar: user.avatar,
+        // 유저 회원가입시에도 id를 세션으로 저장하도록
+        _id: user.data._id,
+      };
+      req.session.save(function () {
+        res.redirect("/");
+      });
     })
-  }).catch((regErrors) => {
-    regErrors.forEach(function(error) {
-      req.flash('regErrors', error)
-    })
-    req.session.save(function() {
-      res.redirect('/')
-    })
-  })
-}
+    .catch((regErrors) => {
+      regErrors.forEach(function (error) {
+        req.flash("regErrors", error);
+      });
+      req.session.save(function () {
+        res.redirect("/");
+      });
+    });
+};
 
-exports.home = function(req, res) {
+exports.home = function (req, res) {
   if (req.session.user) {
-    res.render('home-dashboard')
+    res.render("home-dashboard");
   } else {
-    res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
+    res.render("home-guest", {
+      errors: req.flash("errors"),
+      regErrors: req.flash("regErrors"),
+    });
   }
-}
+};
--- router.js
+++ router.js
@@ -1,27 +1,16 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("./controllers/userController");
-const postController = require("./controllers/postController");
+const express = require('express')
+const router = express.Router()
+const userController = require('./controllers/userController')
+const postController = require('./controllers/postController')
 
 // user related routes
-router.get("/", userController.home);
-router.post("/register", userController.register);
-router.post("/login", userController.login);
-router.post("/logout", userController.logout);
+router.get('/', userController.home)
+router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
 
 // post related routes
-router.get(
-  "/create-post",
-  userController.mustBeLoggedIn,
-  postController.viewCreateScreen
-);
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 
-// /create-post 경로에 대한 post 요청 라우터이다
-router.post(
-  "/create-post",
-  userController.mustBeLoggedIn,
-  // postController 에서 정의해준ㄷ
-  postController.create
-);
-
-module.exports = router;
+module.exports = router
