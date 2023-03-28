b68a46b7f7240665893c095a3e7b0cae11f100ed
SeungOhHong <ghdtmddh@gmail.com>
2023-03-28 14:54:47 +0900

    feat : 세션데이터 재사용을 위해 미들웨어에서 정의하고 로그인 상태에 따라 HTML 띄우기

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,9 @@
 const User = require("../models/User");
 
-// mustBeLoggedIn : 게시물 생성 전에 로그인 된 상태인지 세션을 확인하는 함수
-// 3번째 파라미터로 next를 받는다
 exports.mustBeLoggedIn = function (req, res, next) {
   if (req.session.user) {
-    // 만약 세션 데이터에 user 객체가 있다면(로그인 상태) next() 함수가 실행된다
     next();
   } else {
-    // 아니라면 에러를 표시하고 홈 URL로 리다이렉트한다
-    // 리다이렉트 전에 세션을 save한다
-    // 이제 비로그인 상태에서 /create-post 라우터로 이동하려한다면 에러플레시를 띄워준다
     req.flash("errors", "You must be logged in to perform that action.");
     req.session.save(function () {
       res.redirect("/");
@@ -63,6 +57,9 @@ exports.register = function (req, res) {
 
 exports.home = function (req, res) {
   if (req.session.user) {
+    // username: user.data.username, avatar: user.avatar
+    // 로직이 반복된다 그러므로 app.js 에서 미들웨어로 정의해준다
+
     res.render("home-dashboard");
   } else {
     res.render("home-guest", {
--- router.js
+++ router.js
@@ -1,28 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("./controllers/userController");
-// 포스트 컨트롤러를 임포트해준다
 const postController = require("./controllers/postController");
 
-// 유저와 관련된 라우터
+// user related routes
 router.get("/", userController.home);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 
-// 포스트와 관련된 라우터
-
-// express 에서는 특정 라우터에 대해서 여러개의 함수로 응답할 수 있도록 할 수 있다
-// 현재 /create-post 라우터에 대해서  userController.mustBeLoggedIn, postController.viewCreateScreen
-// 이 두 가지 함수가 실행되도록 정의하였다
+// post related routes
 router.get(
   "/create-post",
-  // 이 함수의 역할은 현재 세션에 user 객체가 있는지 확인하는 것이다
-  // 확인한 후 user 객체가 존재한다면 next() 함수를 통해서 다음 함수를 실행할 수 있다
-  // 즉, 로그인 된 사용자만이 해당 라우터(/create-post)에 접근할 수 있게 되는 것이다
-  // (왜냐하면   res.render("create-post") 가 next 함수인 viewCreateScreen에 정의 돼있으니까)
   userController.mustBeLoggedIn,
-  // 포스트컨트롤러에서 정의할 viewCreateScreen 함수를 참조한다
   postController.viewCreateScreen
 );
 
